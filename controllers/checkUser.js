71d00e011a096375a23aa2a595f04611a0dfb8bf
Devkant Kumar <138906683+Devkant01@users.noreply.github.com>
2024-09-17 11:45:38 +0530

    Update 👍

--- controllers/checkUser.js
+++ controllers/checkUser.js
@@ -2,55 +2,27 @@ const { userModel } = require('../models/userModel');
 
 async function checkUser(req, res, next) {
     const { username, password } = req.body;
-
-    try {
-        const userExists = await userModel.findOne({ username });
-
-        if (!userExists) {
-            return res.status(404).json({
-                title: "User Not Found",
-                statusCode: 404,
-                message: "User Not Found",
-                description: "The username you entered does not exist in our records. Please check your username or sign up for a new account."
-            });
-        }
-
-        if (userExists.password !== password) {
-            return res.status(401).json({
-                title: "Invalid Credentials",
-                statusCode: 401,
-                message: "Unauthorized",
-                description: "The password you entered is incorrect. Please try again or reset your password if you've forgotten it."
-            });
-        }
-
-        next();
-    } catch (error) {
-        console.error('Error in checkUser middleware:', error);
-        res.status(500).json({
-            title: "Server Error",
-            statusCode: 500,
-            message: "An unexpected error occurred",
-            description: "Please try again later."
-        });
+    const userExists = await userModel.findOne({ username });
+    if (!userExists) {
+        return res.render("error", { title: "User Not Found", statusCode: 404, message: "User Not Found", description: "The username you entered does not exist in our records. Please check your username or sign up for a new account." })
     }
+    
+    if (userExists.password !== password) {
+        return res.render("error", { title: "Invalid Credentials", statusCode: 401, message: "Unauthorized", description: "The password you entered is incorrect. Please try again or reset your password if you've forgotten it." })
+    }
+    next();
 }
 
-async function getAllUsers(req, res) {
-    try {
-        const users = await userModel.find({});
-        return res.json({
-            users
-        });
-    } catch (error) {
-        console.error('Error in getAllUsers:', error);
-        return res.status(500).json({
-            title: "Server Error",
-            statusCode: 500,
-            message: "An unexpected error occurred",
-            description: "Please try again later."
-        });
+async function getAllUsers(req, res, next) {
+    const userExists = await userModel.find({ });
+    if (!userExists) {
+        return res.render("error", { title: "User Not Found", statusCode: 404, message: "User Not Found", description: "The username you entered does not exist in our records. Please check your username or sign up for a new account." })
     }
+    
+    return res.json({
+        userExists
+    })
 }
+
 
 module.exports = { checkUser, getAllUsers };
