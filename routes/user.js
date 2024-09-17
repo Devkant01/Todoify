650f0b644ee7cad847386acba58abb02f904b119
Devkant Kumar <138906683+Devkant01@users.noreply.github.com>
2024-09-17 11:22:33 +0530

    Updated 👍

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { loginValidation, signupValidation } = require("../validators/userValidation");
 const { createUser } = require("../controllers/createUser");
 const { checkUser } = require("../controllers/checkUser");
+const { getAllUsets } = require("../controllers/getAllUsets");
 const { signupAuth, loginAuth } = require("../middleware/userAuth");
 const router = express.Router();
 
 router.get('/', (req, res) => { res.send("Hello users, go for login/signup"); });
+
+router.get('/allusers', getAllUsets);
 
 router.post('/login', loginValidation, checkUser, loginAuth);
 
