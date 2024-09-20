7fe7bc1226eceb50ce633fff71a25dd6531045c0
Devkant <devkantsah43@gmail.com>
2024-09-20 21:08:26 +0530

    🎉 Finally completed it! Fully responsive, with both frontend and backend features rock solid 🚀💻🎨🔧

--- controllers/addTodo.js
+++ controllers/addTodo.js
@@ -6,7 +6,7 @@ const { adminModel } = require("../models/admin");
 async function addTodo(req, res, next) {
     try {
         const { title, description } = req.body;
-        const token = req.session.token; //not using express-session anymore
+        const token = req.session.token;
         // const { token } = await adminModel.findOne({ admin: "dev" }, { token: 1, _id: 0 });
         const user = jwt.verify(token, jwt_secret_key);
         const todo = await todosModel.create({
@@ -14,7 +14,7 @@ async function addTodo(req, res, next) {
             title,
             description
         });
-        next();
+        res.redirect("/todoify");
     } catch (e) {
         res.render("error", { title: "Invalid token", statusCode: 400, message: "Invalid token", description: "Please log in again to continue" });
     }
--- middleware/todosAuth.js
+++ middleware/todosAuth.js
@@ -2,19 +2,20 @@ const jwt = require("jsonwebtoken");
 const { jwt_secret_key } = require("../config/config");
 const { todosModel } = require("../models/todosModel");
 const { adminModel } = require("../models/admin");
-const { route } = require("../routes");
 
 async function todosAdded(req, res, next) {
     try {
         const title = req.body.title;
-        const token = req.session.token; //not using express-session
+        const token = req.session.token;
         // const token = await adminModel.findOne({ admin: "dev" }, { token: 1, _id: 0 });
         const user = jwt.verify(token, jwt_secret_key);
-        await todosModel.findOne({ userId: user.userId, title });
-        res.redirect("/todoify");
-        return;
+        const todo = await todosModel.findOne({ userId: user.userId, title });
+        if (todo) {
+            throw new Error("Todo already exists");
+        }
+        next();
     } catch (error) {
-        res.render("error", {route: "/todoify" ,title: "Todo already exists", statusCode: 400, message: "Todo already exists", description: "Please enter a different todo", btnMsg: "BACK TO DASHBOARD" });
+        res.render("error", {route: "/todoify" ,title: error.message, statusCode: 400, message: "Todo already exists", description: "Please enter a different todo", btnMsg: "BACK TO DASHBOARD" });
     }
 }
 
